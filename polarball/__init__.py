6f3c419895c1a7352a05739663f030ea92a06918
grburgess <jmichaelburgess@gmail.com>
2021-10-22 16:06:51 +0200

    basics ready

--- polarball/__init__.py
+++ polarball/__init__.py
@@ -1,9 +1,11 @@
-# -*- coding: utf-8 -*-
+from .polarball import PolarResponse
+
 
 """Top-level package for polarball."""
 
 __author__ = """J. Michael Burgess"""
-__email__ = 'jburgess@mpe.mpg.de'
+__email__ = "jburgess@mpe.mpg.de"
 
 from . import _version
-__version__ = _version.get_versions()['version']
+
+__version__ = _version.get_versions()["version"]
--- polarball/polarball.py
+++ polarball/polarball.py
@@ -1,3 +1,43 @@
-# -*- coding: utf-8 -*-
+import h5py
 
-"""Main module."""
+from rball import ResponseDatabase
+
+
+class PolarResponse(ResponseDatabase):
+    @classmethod
+    def from_hdf5(cls, file_name: str, use_high_gain: bool = False):
+
+        with h5py.File(file_name, "r") as f:
+
+            # this part is a little polar specific at the moment
+            # will remove and generalize
+
+            if use_high_gain:
+
+                ext = "hg"
+
+            else:
+
+                ext = "lg"
+
+            list_of_matrices = f[f"matrix_{ext}"][()]
+
+            theta = f["theta"][()]
+
+            phi = f["phi"][()]
+
+            ebounds = f["ebounds"][()]
+
+            mc_energies = f["mc_energies"][()]
+
+            return cls(
+                list_of_matrices=list_of_matrices,
+                theta=theta,
+                phi=phi,
+                ebounds=ebounds,
+                monte_carlo_energies=mc_energies,
+            )
+
+    def _transform_to_instrument_coordinates(self, ra: float, dec: float):
+
+        return super()._transform_to_instrument_coordinates(ra, dec)
